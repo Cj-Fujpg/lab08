fd9955f0c1e763177d019b1f9ae611f89decb4a6
Cj Fu <105927887@student.swin.edu.au>
2025-05-01 13:42:57 +1000

    testing

--- process.php
+++ process.php
@@ -3,9 +3,10 @@ session_start()
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-if($username=='admin'&& $password == 'password123') {$_SESSION['user'] = $username;
+if($username=='admin'&& $password == 'password123') {
+    $_SESSION['user'] = $username;
 header('Location: welcome.php');
 } else {
-echo "Invalid login. <a href='login.html'>Try again</a>";
+echo "Invalid login". <a href='login.html'>Try again</a>;
 }
 ?>
