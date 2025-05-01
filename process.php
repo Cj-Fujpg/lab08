58f1a29d9b5740c5509646587ca352e28c3865cc
Cj Fu <105927887@student.swin.edu.au>
2025-05-01 13:59:11 +1000

    more changes

--- process.php
+++ process.php
@@ -3,10 +3,11 @@ session_start();
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-if($username=='105927887'&& $password == 'pw123') {
+if($username=='admin'&& $password == 'pw123') {
     $_SESSION['user'] = $username;
-header('Location: welcome.php');
+    header('Location: welcome.php');
+    exit;
 } else {
-echo "Invalid login. <a href='login.html'>Try again</a>";
+    echo "Invalid login. <a href='login.html'>Try again</a>";
 }
 ?>
