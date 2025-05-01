a1b4ee94d9b947239a5de6feb2c777656882ad6c
Cj Fu <105927887@student.swin.edu.au>
2025-05-01 14:10:14 +1000

    edited

--- welcome.php
+++ welcome.php
@@ -1,16 +1,10 @@
 <?php
 session_start();
+include 'header.inc'
+include 'footer.inc'
 if (isset($_SESSION['user'])) {
     echo "Welcome, ".$_SESSION['user'];
 } else {
     header('Location: login.html');
 }
 ?>
-
-
-<?php include 'header.inc'; ?>
-
-<h2>Welcome</h2>
-<p>Login successful! Welcome, <?php echo htmlspecialchars($_POST['username']); ?>.</p>
-
-<?php include 'footer.inc'; ?>
