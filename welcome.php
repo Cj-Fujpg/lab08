f03c87662fd3c1178eb2dc370e222f251c522733
Cj Fu <105927887@student.swin.edu.au>
2025-05-01 14:08:05 +1000

    Testing

--- welcome.php
+++ welcome.php
@@ -6,3 +6,11 @@ if (isset($_SESSION['user'])) {
     header('Location: login.html');
 }
 ?>
+
+
+<?php include 'header.inc'; ?>
+
+<h2>Welcome</h2>
+<p>Login successful! Welcome, <?php echo htmlspecialchars($_POST['username']); ?>.</p>
+
+<?php include 'footer.inc'; ?>
